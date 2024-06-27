3ab4079bb959abd9414f505615ef74130ddeca9e
mayorr-star <immaboat11@gmail.com>
2024-06-27 20:36:26 +0000

    Refactord code to make it shorter

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,8 +1,6 @@
 function capitalize(string) {
   if (typeof(string) !== "string") return "Only strings are accepted";
   if (string.trim().length === 0) return "";
-  const firstChar = string.charAt(0).toUpperCase();
-  const splitStr = string.slice(1);
-  return firstChar.concat(splitStr).join("");
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 module.exports = capitalize;
