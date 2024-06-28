76f0de5f741a1a1a83e0537a284822ecafcfdf30
mayorr-star <immaboat11@gmail.com>
2024-06-28 11:23:24 +0000

    Write function to encrypt data using caesar cipher

---
+++ caesarCipher/caesarCipher.js
@@ -0,0 +1,25 @@
+function caesarCipher(str, shiftFactor = 1) {
+  if (!str || typeof str !== "string") return false;
+  return [...str].map((char, i) => {
+    let charCode = str.codePointAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      charCode += shiftFactor;
+      if (charCode > 90) {
+        charCode = charCode - 90 + 64;
+      } else if (charCode < 65) {
+        charCode = charCode - 65 + 89;
+      }
+    } else if (charCode >= 97 && charCode <= 122) {
+      charCode += shiftFactor;
+      if (charCode > 122) {
+        charCode = charCode - 122 + 96;
+      } else if (charCode < 97) {
+        charCode = charCode - 97 + 121;
+      }
+    }
+    console.log(charCode)
+    return charCode
+  }).map(char => String.fromCharCode(char)).join("");
+}
+console.log(caesarCipher("xyz", 3))
+module.exports = caesarCipher;
