21343e51cd649ff8dfb8ae0325ba3ae77bcff8b2
mayorr-star <immaboat11@gmail.com>
2024-06-28 07:45:08 +0000

    Write a function to return an object with average, max , min and length properties of an array

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,9 +1,10 @@
 function analyzeArray(arr) {
+    if (!Array.isArray(arr)) return false;
     return {
-        average: getAverage(),
-        min: getMinValue(),
-        max: getMaxValue(),
-        length: getLength()
+        average: getAverage(arr),
+        max: getMaxValue(arr),
+        min: getMinValue(arr),
+        length: getLength(arr)
     }
 }
 
@@ -11,3 +12,19 @@ function getAverage(arr) {
     return arr.reduce((prev, curr) => prev + curr, 0) / arr.length;
 }
 
+function getMaxValue(arr) {
+    return arr.reduce((prev, curr) => {
+        return Math.max(prev,curr)
+    }, arr[0])
+}
+
+function getMinValue(arr) {
+    return arr.reduce((prev, curr) => {
+        return Math.min(prev,curr)
+    }, arr[0])
+}
+
+function getLength(arr) {
+    return arr.length;
+}
+module.exports = analyzeArray
